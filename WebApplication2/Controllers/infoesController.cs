ba6bf0173364b5099ec40cc5dfb3f5e673024ca0
arfeen876 <69490894+arfeen876@users.noreply.github.com>
2021-12-28 18:46:00 +0500

    Add project files.

---
+++ WebApplication2/Controllers/infoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class infoesController : Controller
+    {
+        private dafaEntities db = new dafaEntities();
+
+        // GET: infoes
+        public ActionResult Index()
+        {
+            return View(db.info.ToList());
+        }
+
+        // GET: infoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.info.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: infoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "e_id,id,Update_time")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.info.Add(info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(info);
+        }
+
+        // GET: infoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.info.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "e_id,id,Update_time")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.info.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            info info = db.info.Find(id);
+            db.info.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
